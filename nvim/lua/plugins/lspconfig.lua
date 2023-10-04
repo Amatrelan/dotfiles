b73c486b629f64ed27b3fc34cb9897a9f78f3329
Jarmo Riikonen <amatrelan@gmail.com>
2023-10-04 18:20:31 +0300

    rust-analyzer clippy

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -89,6 +89,9 @@ function Plugin.config()
 			["lua_ls"] = function()
 				require("plugins.lsp.lua_ls")
 			end,
+			rust_analyzer = function()
+				require("plugins.lsp.rust_analyzer")
+			end,
 		}
 	})
 end
