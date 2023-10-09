28a3313d38b8a837859f78904a24ef58b3d17397
Jarmo Riikonen <amatrelan@gmail.com>
2023-10-09 10:53:31 +0300

    some updates

--- nvim/lua/plugins/mini-pairs.lua
+++ nvim/lua/plugins/mini-pairs.lua
@@ -2,4 +2,8 @@ local Plugin = { 'echasnovski/mini.pairs' }
 
 Plugin.version = false
 
+function Plugin.config()
+	require("mini.pairs").setup()
+end
+
 return Plugin
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -17,10 +17,8 @@ c.aliases = {
 config.set("colors.webpage.preferred_color_scheme", "dark")
 # config.set("colors.webpage.darkmode.enabled", True)
 
-# config.set("auto_save.session", True)
 c.auto_save.session = True
 
-# config.set("downloads.position", "bottom")
 c.downloads.position = "bottom"
 
 config.bind("<Ctrl-n>", "completion-item-focus next", mode="command")
@@ -40,14 +38,14 @@ c.fileselect.folder.command = ["kitty", "-e", "ranger", "--choosedir={}"]
 c.fileselect.multiple_files.command = ["kitty", "-e", "ranger", "--choosefiles={}"]
 c.fileselect.single_file.command = ["kitty", "-e", "ranger", "--choosefile={}"]
 
-c.content.blocking.method = "both"
-c.content.blocking.adblock.lists = [
-        "https://easylist.to/easylist/easylist.txt",
-        "https://easylist.to/easylist/easyprivacy.txt",
-        "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
-        "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
-        "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
-]
+# c.content.blocking.method = "both"
+# c.content.blocking.adblock.lists = [
+#         "https://easylist.to/easylist/easylist.txt",
+#         "https://easylist.to/easylist/easyprivacy.txt",
+#         "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+#         "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
+#         "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+# ]
 
 c.downloads.location.directory = "~/downloads"
 
@@ -64,6 +62,6 @@ c.url.searchengines = {
 c.content.autoplay = False
 c.content.default_encoding = "utf-8"
 
-c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+# c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
 
 c.content.cookies.accept = "no-3rdparty"
